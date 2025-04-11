fcb7319a22b456e2511b0cdbe9b2fd5727f853f5
sergi.falcon <sergi.falcon@porsche.digital>
2025-04-11 19:59:31 +0200

    changes

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="fixed w-full h-28 z-10 bg-transparent flex justify-center items-center">
+    <img class="h-16 w-16" src="/src/assets/logo.png" alt="Logo">
+  </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+</style>
