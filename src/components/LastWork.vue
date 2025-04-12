9a47aefc59eace3b4c7f4b2cf82d77eeb805f8c2
sergi.falcon <sergi.falcon@porsche.digital>
2025-04-12 02:00:38 +0200

    instaposts

--- src/components/LastWork.vue
+++ src/components/LastWork.vue
@@ -1,17 +1,17 @@
 <template>
   <SectionBanner title="Últimos trabajos" imgUrl="/assets/banner.png"
                  imgAltText="Una persona siendo tatuada en Ondo Barcelona"/>
-  <div class="bg-zinc-900 w-full py-2 md:p-10 flex flex-col justify-between">
-    <div class="max-w-[960px] m-auto grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
+  <div class="bg-zinc-900 w-full p-5 md:p-10 flex flex-col justify-between">
+    <div class="max-w-[960px] m-auto grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-6 p-3 md:p-6">
       <div v-for="work in lastwork" :key="work.id"
            class="flex flex-col items-center p-4">
         <a :href="work.url" target="_blank">
-          <img :src="`/insta/${work.id}.png`" alt="Artist Image" class="w-[12em] h-[16em] object-contain">
+          <img :src="`/insta/${work.id}.png`" alt="Artist Image" class="w-[13em] h-[16em] object-contain">
         </a>
       </div>
     </div>
     <a href="https://www.instagram.com/ondotattoo/" target="_blank" rel="noopener noreferrer"
-       class="mx-auto text-2xl my-0 border-2 border-white text-white hover:text-zinc-900 px-4 py-2 hover:bg-white">
+       class="mx-auto text-2xl mt-0 mb-5 border-2 border-white text-white hover:text-zinc-900 px-4 py-2 hover:bg-white">
       Siguenos en Instagram
     </a>
   </div>
