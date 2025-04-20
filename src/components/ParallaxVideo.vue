60def2676c86856386180d1da8093d316146654f
sergi.falcon <sergi.falcon@porsche.digital>
2025-04-20 23:36:40 +0200

    ondo videos

--- src/components/ParallaxVideo.vue
+++ src/components/ParallaxVideo.vue
@@ -53,15 +53,15 @@ onUnmounted(() => {
 
 const videos = [
   {
-    src: '/assets/video.mp4',
+    src: '/assets/ondo1.mov',
     id: '1'
   },
   {
-    src: '/assets/video2.mp4',
+    src: '/assets/ondo2.mov',
     id: '2'
   },
   {
-    src: '/assets/video.mp4',
+    src: '/assets/ondo3.mov',
     id: '3'
   }
 ]
