c71ae0ff98a16dfe886215b91ed65d7d5d8f4d88
sergi.falcon <sergi.falcon@porsche.digital>
2025-08-05 13:23:45 +0200

    improve navbar seo

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -5,6 +5,7 @@
     </div>
     <div class="relative z-50 map-container h-[450px]  w-full flex justify-center align-center">
       <iframe
+          title="Google Maps - Ondo Tattoo Barcelona"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5988.2847105113415!2d2.146508776669166!3d41.37099177130188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4a27758e88cab%3A0x88679085114a4592!2sOndo%20Tattoo%20Barcelona!5e0!3m2!1ses!2ses!4v1745251471736!5m2!1ses!2ses"
           width="600"
           height="450"
